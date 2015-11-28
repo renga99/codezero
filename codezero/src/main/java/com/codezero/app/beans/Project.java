7a26a5c0571f07ef7fcae4f2118e375062784812
Rengaraj Selvaraj <renga99@gmail.com>
2015-11-29 01:04:27 +0530

    updated project structure and app index page test.html

--- codezero/src/main/java/com/codezero/app/beans/Project.java
+++ codezero/src/main/java/com/codezero/app/beans/Project.java
@@ -2,4 +2,28 @@ package com.codezero.app.beans;
 
 public class Project {
 
+	private Integer id;
+	private String description;
+	private String name;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
---
+++ codezero/src/main/resources/codezero.sql
@@ -0,0 +1,25 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+CREATE SCHEMA IF NOT EXISTS `codezero` DEFAULT CHARACTER SET utf8 ;
+USE `codezero` ;
+
+-- -----------------------------------------------------
+-- Table `codezero`.`project`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `codezero`.`project` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `description` VARCHAR(300) NOT NULL ,
+  `name` TEXT NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB
+AUTO_INCREMENT = 41
+DEFAULT CHARACTER SET = utf8;
+
+USE `codezero` ;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
